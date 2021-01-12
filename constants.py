5aebc3b3d964018fd42665970ddfd29048c562f8
kf7lsu <mtrahms@gmail.com>
2021-01-11 16:49:06 -0800

    revised model structure for better FINN compat, see quant_modelV2

--- constants.py
+++ constants.py
@@ -3,12 +3,12 @@ import os
 DEFAULT_SPLIT = (0.6, 0.3, 0.1)
 
 BATCH_SIZE = 500
-N_EPOCHS = 500
-LEARNING_RATE = 0.01
+N_EPOCHS = 100
+LEARNING_RATE = 0.03
 
 DATA_FOLDER_PATH = "data"
 MODELS_FOLDER_PATH = "models"
 BEST_LOSS_PATH = os.path.join(MODELS_FOLDER_PATH, "min_ave_val_loss_quant.txt")
 
-WEIGHT_BW = 8
-ACT_BW = 8
+WEIGHT_BW = 4
+ACT_BW = 4
---
+++ quant_modelV2.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from brevitas.nn import QuantLinear, QuantReLU, QuantIdentity
+from constants import WEIGHT_BW, ACT_BW
+
+class QuantNet_opt(nn.Module):
+    def __init__(self, n_features):
+        super(QuantNet_opt, self).__init__()
+        self.bn0 = nn.BatchNorm1d(n_features, momentum=0.6)
+        self.relu0 = QuantReLU(bit_width=ACT_BW)
+        self.fc1 = QuantLinear(n_features, 31, bias=True, weight_bit_width=WEIGHT_BW)
+        self.bn1 = nn.BatchNorm1d(31, momentum=0.6)
+        self.relu1 = QuantReLU(bit_width=ACT_BW)
+        self.fc2 = QuantLinear(31, 11, bias=True, weight_bit_width=WEIGHT_BW)
+        self.bn2 = nn.BatchNorm1d(11, momentum=0.6)
+        self.relu2 = QuantReLU(bit_width=ACT_BW)
+        self.fc3 = QuantLinear(11, 3, bias=True, weight_bit_width=WEIGHT_BW)
+        self.bn3 = nn.BatchNorm1d(3, momentum=0.6)
+        self.relu3 = QuantReLU(bit_width=ACT_BW)
+        self.fc4 = QuantLinear(3, 1, bias=True, weight_bit_width=WEIGHT_BW)
+        self.bn4 = nn.BatchNorm1d(1, momentum=0.6)
+        self.relu4 = QuantReLU(bit_width=ACT_BW)
+
+    def forward(self, x):
+        x = self.bn0(x)
+        x = self.relu0(x)
+        x = self.fc1(x)
+        x = self.bn1(x)
+        #x = F.relu(self.fc1(x))
+        x = self.relu1(x)
+        #x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        #x = F.relu(self.fc2(x))
+        x = self.relu2(x)
+        x = self.fc3(x)
+        x = self.bn3(x)
+        #x = F.relu(self.fc3(x))
+        x = self.relu3(x)
+        x = self.fc4(x)
+        x = self.bn4(x)
+        #x = F.relu(self.fc4(x))
+        x = self.relu4(x)
+        return x
